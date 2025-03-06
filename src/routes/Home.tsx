04fae08aab4db2e635488e7f1281b8625e068f12
Alimedhat000 <alisensei000@gmail.com>
2025-03-06 21:21:31 +0200

    feat: enhance HeroBanner layout and integrate AnnouncementBar with dynamic props

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,13 +1,31 @@
 import HeroBanner from '../components/Home/HeroBanner';
-// import AnnouncementBar from "../components/NavBar/AnouncementBar";
+import AnnouncementBar from '../components/NavBar/AnnouncementBar';
+import { AnnouncementBarProps } from '../components/NavBar/types';
 
 function Home() {
+  const announcementbar: AnnouncementBarProps = {
+    items: [
+      { text: 'NEW ARRIVALS', link: '/' },
+      { text: 'NEW ARRIVALS', link: '/' },
+      { text: 'NEW ARRIVALS', link: '/' },
+      { text: 'NEW ARRIVALS', link: '/' },
+    ],
+    speed: 80,
+    direction: 'left',
+    showDots: false,
+    itemClassName: 'text-black text-6xl py-15',
+    className: 'bg-gray-100 px-0',
+  };
   return (
     <>
       <div className="flex w-full flex-col items-center justify-center">
         <HeroBanner />
 
-        {/* <AnnouncementBar />
+        <div className="w-full overflow-hidden">
+          <AnnouncementBar {...announcementbar} />
+        </div>
+
+        {/*
         <ProductSlider />
         <ProductGrid /> */}
       </div>
