aff4af11eafa07e33293a8accdbece14f5146eba
Alimedhat000 <alisensei000@gmail.com>
2025-04-13 22:46:43 +0200

    Initial Product Page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import Layout from './components/Layout';
 import Home from './routes/Home';
 import Collections from './routes/Collections';
-import Product from './routes/Product';
+import ProductPage from './routes/Product';
 import Cart from './routes/Cart';
 import Error404 from './routes/Error404';
 import { Navigate } from 'react-router-dom';
@@ -13,7 +13,7 @@ const routes = {
     { path: '', element: <Home /> },
     { path: 'collections', element: <Navigate to="/collection/all" replace /> }, // 👈 redirect
     { path: 'collections/:category', element: <Collections /> },
-    { path: 'product/:id', element: <Product /> },
+    { path: 'product/:id', element: <ProductPage /> },
     { path: 'cart', element: <Cart /> },
     {
       path: '*',
