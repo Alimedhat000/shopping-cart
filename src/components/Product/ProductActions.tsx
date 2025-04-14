c0d5fda33e2d1c0185910758a20e76992613713c
Alimedhat000 <alisensei000@gmail.com>
2025-04-14 17:09:10 +0200

    finxed font on ProductActions

--- src/components/Product/ProductActions.tsx
+++ src/components/Product/ProductActions.tsx
@@ -9,16 +9,16 @@ export default function ProductActions({
   onBuyNow: () => void;
 }) {
   return (
-    <div className="flex flex-col gap-4 sm:flex-row">
+    <div className="font-condensed flex flex-col gap-4 sm:flex-row">
       <Button
         onClick={onAddToCart}
-        className="bg-primary hover:bg-primary/80 h-12 flex-1 rounded-3xl text-white"
+        className="bg-primary text-md hover:bg-primary/80 h-12 flex-1 rounded-3xl text-white"
       >
         Add to cart
       </Button>
       <Button
         onClick={onBuyNow}
-        className="h-12 flex-1 rounded-3xl bg-black text-white hover:bg-black/90"
+        className="text-md h-12 flex-1 rounded-3xl bg-black text-white hover:bg-black/90"
       >
         Buy it now
       </Button>
