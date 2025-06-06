e7c7b11a523713449f63a9950a900c8187d0f657
Alimedhat000 <alisensei000@gmail.com>
2025-06-06 23:57:16 +0300

    fix: filter component in smaller screens and replaced it with a product count

--- src/routes/Collections.tsx
+++ src/routes/Collections.tsx
@@ -29,11 +29,11 @@ function Collections() {
   return (
     <div className="mb-20">
       <div className="mb-20">
-        <h1 className="pt-20 text-center text-3xl text-black md:text-4xl">
+        <h1 className="hidden pt-20 text-center text-3xl text-black md:block md:text-4xl">
           NEW ARRIVALS
         </h1>
       </div>
-      <div className="site-padding grid grid-cols-1 grid-rows-[20px_1fr] gap-12 md:grid-cols-[250px_1fr] lg:grid-cols-[300px_1fr]">
+      <div className="site-padding grid grid-cols-1 grid-rows-[20px_1fr] md:grid-cols-[250px_1fr] md:gap-12 lg:grid-cols-[300px_1fr]">
         {/* Top row */}
         <div className="col-span-full hidden grid-cols-[inherit] gap-[inherit] sm:col-span-2 md:grid">
           <div className="flex items-center gap-2">
@@ -53,21 +53,26 @@ function Collections() {
         </div>
 
         {/* Main content*/}
+        <FilterBar
+          activeFilters={activeFilters}
+          handleFilterChange={handleFilterChange}
+          className="hidden md:block"
+        />
+        
         {
-          <FilterBar
-            activeFilters={activeFilters}
-            handleFilterChange={handleFilterChange}
-          />
+          <div className="font-condensed -mt-5 text-center text-lg md:hidden">
+            192 products
+          </div> //Todo: This should be dynamic based on the product count
         }
 
         <ProductGrid
           columns={{ sm: 2, md: 2, lg: 4, xl: 4 }}
           columnsClassName="grid gap-2 md:gap-6 
-    grid-cols-2
-    sm:grid-cols-2
-    md:grid-cols-2
-    lg:grid-cols-4
-    xl:grid-cols-4 "
+                            grid-cols-2
+                            sm:grid-cols-2
+                            md:grid-cols-2
+                            lg:grid-cols-4
+                            xl:grid-cols-4 "
           showViewAll={false}
           title=""
           items={12}
